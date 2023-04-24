11e7b4449b3387c20f12efd9a5d05e2371b5a865
Mohamedsaad55 <Mohamedsaad974@rocketmail.com>
2023-04-24 18:11:21 +0000

    Updates

--- print_p.c
+++ print_p.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * this function prints the Percentage character
- * Returns the number of the chars
- */
+* this function prints the Percentage character
+* Returns the number of the chars
+*/
 
-int_print_p(va_list args)
+int _print_p(va_list args)
 {
 	putchar('%');
 	return 1;
