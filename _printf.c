ebf7effb591d4fe9cada05b97edae20ccac9b181
Mohamed Saad <91989921+Mohamedsaad55@users.noreply.github.com>
2023-04-24 19:40:49 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,54 +1,53 @@
-#include<main.h>
+#include "main.h"
+
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- * Description: this function will call function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
+ *
+ * Description: This function will call function that will determine which
+ * printing function to call depending on the conversion specifiers contained
+ * into fmt.
+ *
  * Return: length of the formatted output string
  */
-
-in _printf(const chat *format,...)
+int _printf(const char *format, ...)
 {
 	va_list args;
-	int i =0;
-	int len =0;
+	int i = 0;
+	int len = 0;
 
-	va_start(args,format);
+	va_start(args, format);
 
-	if(format-- NULL ||(format[0] == '%' && format[i] =='\0'))
-		return(-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	// logic Block
-	
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			i++
-			
-		switch (format[i])
-	       	{
+			i++;
+
+			switch (format[i])
+			{
 			case 'c':
 				len += _print_c(args);
 				break;
 			case 's':
-				len += _print_str(args);	
+				len += _print_str(args);
 				break;
 			case '%':
 				len += _print_p(args);
 				break;
+			}
 		}
-
-
-	}
-		else 
+		else
 		{
-			putchar(format[i];
-			len++
+			putchar(format[i]);
+			len++;
 		}
-	i++;
-}
+		i++;
+	}
+
 	va_end(args);
 	return len;
 }
