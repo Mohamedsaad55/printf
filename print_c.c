5c47aacd41de407a333b6d89d5ad976a9cd43be6
Heba elborie <hobalittleprinces@hotmail.com>
2023-04-23 19:57:18 -0700

    print_c.c

---
+++ print_c.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * printf_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int _print_char(va_list rgs)
+{
+	int c= va_args(args, int);
+	putchar(c);
+	return 1;
+}
