febaa5d4e5c155112921c6198a0f0d6c4f7968d0
Mohamedsaad55 <Mohamedsaad974@rocketmail.com>
2023-04-24 17:52:23 +0000

    Update

--- print_str.c
+++ print_str.c
@@ -8,16 +8,16 @@
 int _print_str(va_list args)
 {
 	char*str = va_arg(args,char*);
-	int lenght = 0;
+	int length = 0;
 	int i=0;
 	
 	for(i = 0 ; str[i] != '\0' ;i++)
 	{
-		putchar(s[i]);
-		len++;
+		putchar(str[i]);
+		length++;
 	}
 
-	return len;
+	return length;
 
 }
 
