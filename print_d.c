0eff235ecf133265f09598ac79aea1408a3f4e55
Heba elborie <hobalittleprinces@hotmail.com>
2023-04-25 10:38:38 -0700

    Update

--- print_d.c
+++ print_d.c
@@ -4,14 +4,14 @@
  @args: argument to print
 Return: number of characters printed
  */
-int _print_d (va_list args)
+int _print_d(va_list args)
 {
 	int x = va_arg(args, int);
 	int l = 0;
 	int s = 0;
 	if (x<0)
 	{
-		s =1;
+		s = 1;
 		x* = -1;
 		_putchar('-');
 			l++;
